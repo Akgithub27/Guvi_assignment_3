3f7ff3ca36b04a11afa9560f92547afb4f0c164f
AakashPrakash <aakashprakash27@gmail.com>
2025-11-04 00:24:42 +0530

    program 1 book management system

---
+++ BookManagementSystem/BookManagementSystem.java
@@ -0,0 +1,29 @@
+package BookManagementSystem;
+
+public class BookManagementSystem {
+    public static void main(String[] args) {
+        Library library = new Library();
+
+        Book firstBook = new Book(1001,"ramayana","valimiki",true);
+        Book secondBook = new Book(1002,"half girl friend","chetanbagath",true);
+        Book thirdBook = new Book(1003,"rich dad poor dad","robert",true);
+        Book fourthBook = new Book(1004,"the time keeper","mitch albom",true);
+        Book fifthBook = new Book(1005,"The Psychology of Money","Morgan House",true);
+        Book replaceBook = new Book("Ikigai","Francesc Miralles",true);
+
+        library.addBook(firstBook);
+        library.addBook(secondBook);
+        library.addBook(thirdBook);
+        library.addBook(fourthBook);
+        library.addBook(fifthBook);
+
+
+        library.displayAllBooks();
+
+        System.out.println("BookManagementSystem.Book that you searched : "+library.searchBook(1004));
+
+        library.replaceBook(1003,replaceBook);
+
+        library.displayAllBooks();
+    }
+}
---
+++ BookManagementSystem/Library.java
@@ -0,0 +1,49 @@
+package BookManagementSystem;
+
+public class Library {
+    Book books[];
+    int count=0;
+
+    public Library(){
+        this.books = new Book[5];
+    }
+
+    public void addBook(Book book){
+        if(count<=4){
+            this.books[count]=book;
+            count++;
+        }else{
+            System.out.println("BookManagementSystem.Library is full");
+        }
+    }
+
+    public void displayAllBooks(){
+        System.out.println("========================================================================================");
+        System.out.println("List of books");
+        for(Book obj: this.books){
+            System.out.println(obj.toString());
+        }
+        System.out.println("========================================================================================");
+    }
+
+    public String searchBook(int bookID){
+        for(Book obj: this.books){
+            if(obj.isAvailable() && obj.getBookID() == bookID){
+                return obj.toString();
+            }
+        }
+        return "BookManagementSystem.Book is not available or you had entered wrong book ID";
+    }
+
+    public void replaceBook(int bookId,Book book){
+        for(int i=0;i<this.books.length;i++){
+            if(this.books[i].getBookID() == bookId){
+                this.books[i].setBookID(bookId);
+                this.books[i].setAuthor(book.getAuthor());
+                this.books[i].setTitle(book.getTitle());
+                this.books[i].setAvailable(book.isAvailable());
+            }
+        }
+        System.out.println("BookManagementSystem.Book replaced successfully");
+    }
+}
